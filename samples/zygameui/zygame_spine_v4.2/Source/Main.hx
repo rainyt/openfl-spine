8668444409919f5e76096031a5b0c80093a14fae
Rainy <939029835@qq.com>
2024-10-12 11:33:58 +0800

    新增zygameui引擎的测试例子

---
+++ samples/zygameui/zygame_spine_v4.2/Source/Main.hx
@@ -0,0 +1,15 @@
+package;
+
+    import zygame.core.Start;
+    
+    class Main extends Start {
+        public function new() {
+            super(1920 ,1080 ,false);
+        }
+    
+        override function onInit() {
+            super.onInit();
+            // 代码初始化入口
+        }
+    }
+    
